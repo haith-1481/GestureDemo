a32f942a063b7ee6e56e871c9069b15a2564aaeb
oTrinhHoangHai <trinh.hoang.hai@framgia.com>
2019-04-16 08:23:06 +0700

    Initial Commit

---
+++ UIRecognizerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIRecognizerDemo
+//
+//  Created by trinh.hoang.hai on 4/16/19.
+//  Copyright © 2019 trinh.hoang.hai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
